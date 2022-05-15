b4983983c5fefe6679d04e46f298a7504b7b3ef6
Mohammad Wildan <98458241+mohwildan@users.noreply.github.com>
2022-05-15 15:15:51 +0700

    Delete Globalstyle.js

--- Globalstyle.js
+++
@@ -1,9 +0,0 @@
-import {createGlobalStyle} from "styled-components"
-import "@fontsource/source-sans-pro"
-
-
-export const Globalstyle = createGlobalStyle`
- body{
-     font-family: "Source Sans Pro", sans-serif;
- }
-`
